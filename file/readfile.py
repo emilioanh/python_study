65102e60955f7f6db282187204ab9a18da99a579
desmondanh <desmondanh@users.noreply.github.com>
2018-01-08 15:28:35 +0700

    Add files via upload

---
+++ file/readfile.py
@@ -0,0 +1,9 @@
+from string import ascii_lowercase
+from collections import Counter
+"""
+this is to count the number of each letter in the file
+"""
+with open('ques.txt') as f:
+    print (Counter(letter for line in f 
+                  for letter in line.lower() 
+                  if letter in ascii_lowercase))
---
+++ file/writefile.py
@@ -0,0 +1,22 @@
+while True:
+    try:
+        a=int(input('enter the first number:'))
+        b=int(input('enter the second number:'))
+        print("a+b= %i"% (a+b))
+        # print(f"Hi {name} you'll be 100 in {year_100}!")
+        break
+    except ValueError as e:
+        print("Oops!  That was no valid number.  Try again...")
+
+f = open("ques.txt","w")
+c=a+b
+print("Hello Bitch the answer is %i"%c, file=f)
+print("Hello World", file=f)
+"""
+this is not code
+this is shit
+"""
+print("This is our new text f", file=f) 
+print("and this is another line.", file=f) 
+f.write("Why? Because we can.") 
+f.close() 

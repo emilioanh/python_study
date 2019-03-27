a442b10ad02a0c2132344b3b7601cf43955c46ec
2CS\anhnguyen <nguyenvuducanh.96@gmail.com>
2019-03-27 13:09:40 +0700

    setting for debug in local files (ML/flask test) add pylint and formatter add code to loop through folder files

--- ML/flask test/app.py
+++ ML/flask test/app.py
@@ -1,16 +1,25 @@
 from flask import Flask
+# import os
 import tensorflow as tf
 
 app = Flask(__name__)
 
+# extra_dirs = ['E:\2CS\Intern\python_study\ML\flask test\app.py',]
+# extra_files = extra_dirs[:]
+# for extra_dir in extra_dirs:
+#     for dirname, dirs, files in os.walk(extra_dir):
+#         for filename in files:
+#             filename = os.path.join(dirname, filename)
+#             if os.path.isfile(filename):
+#                 extra_files.append(filename)
 
 @app.route('/')
-@app.route('/hello')
+# @app.route('/hello')
 def hello():
-    hello = tf.constant('Hello, TensorFlow! cccc def')
+    hello = tf.constant('Hello, Tnsow! ddaa')
     sess = tf.Session()
     return sess.run(hello)
     # return 'Hello ajinomoto'
 
 # if __name__ == '__main__':
-#     app.run(debug=True, host = '0.0.0.0', port=5000)
+#     app.run(host = '0.0.0.0', port=5000)
